13f7a178e3fca3ed01b938daf8ba9d23d496a48c
DEsMONd99999 <desmond99999@mail.ru>
2016-11-08 21:51:46 +0200

    Add files via upload

--- 4-0.cpp
+++ 4-0.cpp
@@ -1,7 +1,9 @@
-#include <iostream> 
+﻿#include <iostream> 
 #include <iomanip> 
 
 using namespace std;
+
+/* 0) Построить график функции y = sin(2x) / x на промежутке[-6, 6] с шагом 0.1 */
 
 void function();
 
